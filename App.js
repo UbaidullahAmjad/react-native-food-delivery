0a93d3cf1b5e115e5a9461cffe989f5b7dd3caf7
Aqdas Ilyas <73228833+aqdas-ilyas@users.noreply.github.com>
2022-12-14 17:29:35 +0500

    upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BearNavigator from './navigation/BearNavigator';
+import configureStore from './redux/reducers/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
+const App = (props) => {
+  return (
+    <Provider store={store}>
+      <BearNavigator />
+    </Provider>
+  );
+};
+
+export default App;
